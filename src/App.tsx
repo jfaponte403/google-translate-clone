23680582b161a5ea311bf03a180483184e06169a
jfaponte403 <jfaponte403@gmail.com>
2025-06-16 18:47:20 -0500

    Add useReducer for state management in App component
    - Introduced `useReducer` hook to manage translation state.
    - Implemented actions for setting languages, input text, and result.
    - Updated `App.tsx` to initialize and use state with `useReducer`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Button } from 'react-bootstrap';
+import { useReducer } from 'react';
 
 const initialState = {
   fromLanguage: 'auto',
@@ -10,10 +11,53 @@ const initialState = {
 };
 
 function reducer(state, action) {
+  const { type, payload } = action;
 
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    };
+  }
+
+  if (type === "SET_FROM_LANGUAGE") {
+    return {
+      ...state,
+      fromLanguage: payload,
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGE") {
+    return {
+      ...state,
+      toLanguage: payload,
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      fromText: payload,
+      loading: true,
+      result: '',
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      result: payload,
+      loading: false,
+    };
+  }
+
+  return state;
 }
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
       <Button>Hola</Button>
